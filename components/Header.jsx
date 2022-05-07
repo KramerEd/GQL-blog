ff5284ad5ddfb5880e3efc131ad497e64b42f734
KramerEd <danat7531@gmail.com>
2022-05-07 20:28:11 +0300

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import Link from 'next/link'
+
+const cats = [
+  { name: 'React', slug: 'react' },
+  { name: 'webdev', slug: 'fdsfs' },
+]
+
+const Header = () => {
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <div className="inline-block w-full border-b py-8">
+        <div className="block md:float-left">
+          <Link href="/">
+            <span className="cursor-pointer text-4xl font-bold">
+              Graph-blog
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {cats.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold  md:float-right">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
