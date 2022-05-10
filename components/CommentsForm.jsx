1d48711a544cd1659ce69319425046ff8073803b
KramerEd <danat7531@gmail.com>
2022-05-10 19:10:00 +0300

    make a comment form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,8 +1,114 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { submitComment } from '../services'
 
 const CommentsForm = () => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const commentEl = useRef()
+  const nameEl = useRef()
+  const emailEl = useRef()
+  const storeDataEl = useRef()
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name')
+    emailEl.current.value = window.localStorage.getItem('email')
+  }, [])
+
+  const handleCommentsSubmition = () => {
+    setError(false)
+
+    const { value: comment } = commentEl.current
+    const { value: name } = nameEl.current
+    const { value: email } = emailEl.current
+    const { checked: storeData } = storeDataEl.current
+
+    if (!comment || !name || !email) {
+      setError(true)
+      return
+    }
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    }
+    if (storeData) {
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('email', email)
+    } else {
+      window.localStorage.removeItem('name', name)
+      window.localStorage.removeItem('email', email)
+    }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true)
+      setTimeout(() => {
+        setShowSuccessMessage(false)
+      }, 3000)
+    })
+  }
+
   return (
-    <div>CommentsForm</div>
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Comments</h3>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <textarea
+          ref={commentEl}
+          className="te w-full rounded-lg bg-gray-100 p-4 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <input
+          ref={nameEl}
+          type="text"
+          className="w-full rounded-lg bg-gray-100 p-4 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          ref={emailEl}
+          type="text"
+          className="te w-full rounded-lg bg-gray-100 p-4 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <div className="container">
+          <input
+            ref={storeDataEl.el}
+            type="checkbox"
+            value="true"
+            id="storeData"
+            name="storeData"
+            className="cursor-pointer"
+          />
+          <label htmlFor="storeData" className="ml-2 cursor-pointer">
+            Save email for next comment
+          </label>
+        </div>
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are required.</p>
+      )}
+      <div className="mt-8 flex justify-center">
+        <button
+          type="button"
+          onClick={handleCommentsSubmition}
+          className="rounded-100 ease rounded-lg bg-black p-2 px-5 font-medium  text-white transition duration-200 hover:scale-110 hover:bg-gray-300 hover:text-black"
+        >
+          Send
+        </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-3 text-xl font-semibold text-green-500">
+            Comment submitted for review
+          </span>
+        )}
+      </div>
+    </div>
   )
 }
 
